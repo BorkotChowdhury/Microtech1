cf435c241e55fde3c99ad10fbedb5deb41ecb9ca
borkotchowdhury <md.chowdhury.qa@gmail.com>
2020-11-14 20:17:34 -0500

    new push - borkot

---
+++ src/test/java/Search/Google_Search.java
@@ -0,0 +1,24 @@
+package Search;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Google_Search {
+		
+		@Test
+		public void Verify_Search() throws Exception {
+			
+			System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+			ChromeDriver driver = new ChromeDriver();
+			driver.get("https://www.google.com/");
+			driver.manage().window().maximize();
+			Thread.sleep(5000);
+			driver.findElement(By.xpath("//input[name='q'")).click();
+			driver.findElement(By.xpath("//input[name='q'")).sendKeys("Dhaka wiki");
+			driver.findElement(By.xpath("//input[name='btnK'")).submit();
+			
+			
+	  }
+
+}
